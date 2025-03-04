933cde30d2453c07bdd35e852abdd69cecb77b90
referentetic <gabrielpcelectronica@gmail.com>
2025-03-04 07:42:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 // Registrar el Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('https://referentetic.github.io/bitacora/service-worker.js')
+        navigator.serviceWorker.register('/service-worker.js')
             .then((registration) => {
                 console.log('Service Worker registrado con éxito:', registration);
             })
