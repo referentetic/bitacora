776587b88637c60fdc8efa686a529f97d0f3aa20
referentetic <gabrielpcelectronica@gmail.com>
2025-03-04 07:48:09 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-
-// Registrar el Service Worker
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then((registration) => {
-                console.log('Service Worker registrado con éxito:', registration);
-            })
-            .catch((error) => {
-                console.log('Error al registrar el Service Worker:', error);
-            });
+    navigator.serviceWorker.register('/bitacora/service-worker.js', {
+        scope: '/bitacora/'  // Asegúrate de que el scope sea la misma carpeta o subcarpeta donde está el service-worker.js
+    }).then(function(registration) {
+        console.log('Service Worker registrado con éxito:', registration);
+    }).catch(function(error) {
+        console.log('Error al registrar el Service Worker:', error);
     });
 }
 
